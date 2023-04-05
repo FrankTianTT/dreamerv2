8867817025fca53144c01dff518a75132c1bb230
frank <franktian424@qq.com>
2023-04-05 10:55:26 +0800

    :tada: update env to newest and eliminate warning

--- test/mdp.py
+++ test/mdp.py
@@ -9,6 +9,7 @@ from dreamerv2.training.config import MinAtarConfig
 from dreamerv2.training.trainer import Trainer
 from dreamerv2.training.evaluator import Evaluator
 
+
 def main(args):
     wandb.login()
     env_name = args.env
@@ -16,7 +17,7 @@ def main(args):
 
     '''make dir for saving results'''
     result_dir = os.path.join('results', '{}_{}'.format(env_name, exp_id))
-    model_dir = os.path.join(result_dir, 'models')                                                  #dir to save learnt models
+    model_dir = os.path.join(result_dir, 'models')  # dir to save learnt models
     os.makedirs(model_dir, exist_ok=True)
 
     np.random.seed(args.seed)
@@ -26,13 +27,13 @@ def main(args):
         torch.cuda.manual_seed(args.seed)
     else:
         device = torch.device('cpu')
-    print('using :', device)  
-    
+    print('using :', device)
+
     env = OneHotAction(GymMinAtar(env_name))
     obs_shape = env.observation_space.shape
     action_size = env.action_space.shape[0]
-    obs_dtype = bool
-    action_dtype = np.float32
+    obs_dtype = np.dtype(bool)
+    action_dtype = np.dtype(np.float32)
     batch_size = args.batch_size
     seq_len = args.seq_len
 
@@ -40,11 +41,11 @@ def main(args):
         env=env_name,
         obs_shape=obs_shape,
         action_size=action_size,
-        obs_dtype = obs_dtype,
-        action_dtype = action_dtype,
-        seq_len = seq_len,
-        batch_size = batch_size,
-        model_dir=model_dir, 
+        obs_dtype=obs_dtype,
+        action_dtype=action_dtype,
+        seq_len=seq_len,
+        batch_size=batch_size,
+        model_dir=model_dir,
     )
 
     config_dict = config.__dict__
@@ -64,16 +65,16 @@ def main(args):
         scores = []
         best_mean_score = 0
         best_save_path = os.path.join(model_dir, 'models_best.pth')
-        
-        for iter in range(1, trainer.config.train_steps):  
-            if iter%trainer.config.train_every == 0:
+
+        for iter in range(1, trainer.config.train_steps):
+            if iter % trainer.config.train_every == 0:
                 train_metrics = trainer.train_batch(train_metrics)
-            if iter%trainer.config.slow_target_update == 0:
-                trainer.update_target()                
-            if iter%trainer.config.save_every == 0:
+            if iter % trainer.config.slow_target_update == 0:
+                trainer.update_target()
+            if iter % trainer.config.save_every == 0:
                 trainer.save_model(iter)
             with torch.no_grad():
-                embed = trainer.ObsEncoder(torch.tensor(obs, dtype=torch.float32).unsqueeze(0).to(trainer.device))  
+                embed = trainer.ObsEncoder(torch.tensor(obs, dtype=torch.float32).unsqueeze(0).to(trainer.device))
                 _, posterior_rssm_state = trainer.RSSM.rssm_observe(embed, prev_action, not done, prev_rssmstate)
                 model_state = trainer.RSSM.get_model_state(posterior_rssm_state)
                 action, action_dist = trainer.ActionModel(model_state)
@@ -81,24 +82,24 @@ def main(args):
                 action_ent = torch.mean(action_dist.entropy()).item()
                 episode_actor_ent.append(action_ent)
 
-            next_obs, rew, done, _ = env.step(action.squeeze(0).cpu().numpy())
+            next_obs, rew, done, timeout, _ = env.step(action.squeeze(0).cpu().numpy())
             score += rew
 
             if done:
                 trainer.buffer.add(obs, action.squeeze(0).cpu().numpy(), rew, done)
                 train_metrics['train_rewards'] = score
-                train_metrics['action_ent'] =  np.mean(episode_actor_ent)
+                train_metrics['action_ent'] = np.mean(episode_actor_ent)
                 wandb.log(train_metrics, step=iter)
                 scores.append(score)
-                if len(scores)>100:
+                if len(scores) > 100:
                     scores.pop(0)
                     current_average = np.mean(scores)
-                    if current_average>best_mean_score:
-                        best_mean_score = current_average 
+                    if current_average > best_mean_score:
+                        best_mean_score = current_average
                         print('saving best model with mean score : ', best_mean_score)
                         save_dict = trainer.get_save_dict()
                         torch.save(save_dict, best_save_path)
-                
+
                 obs, score = env.reset(), 0
                 done = False
                 prev_rssmstate = trainer.RSSM._init_rssm_state(1)
@@ -113,8 +114,8 @@ def main(args):
     '''evaluating probably best model'''
     evaluator.eval_saved_agent(env, best_save_path)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     """there are tonnes of HPs, if you want to do an ablation over any particular one, please add if here"""
     parser = argparse.ArgumentParser()
     parser.add_argument("--env", type=str, help='mini atari env name')
